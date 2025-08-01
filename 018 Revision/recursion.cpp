23e9c55c58a1cd454900eb1fffd144639bd65c23
adityap5 <aditya29pippal@gmail.com>
2025-08-01 21:30:52 +0530

    Add recursive functions for checking if an array is sorted, calculating the sum of an array, linear search, and binary search in recursion.cpp; update main function to demonstrate these functionalities and include corresponding executable file.

--- 018 Revision/recursion.cpp
+++ 018 Revision/recursion.cpp
@@ -39,15 +39,58 @@ int climbStairs(int n){
     return climbStairs(n-1) + climbStairs(n-2);
 }
 
+//is sorted
+bool isSorted(int arr[],int size){
+    if(size==0 || size==1) return true;
+    if(arr[0]>arr[1]) return false;
+    else{
+        return isSorted(arr+1,size-1);
+    }
+}
+//array sum
+int arraysum(int arr[],int size){
+    if(size==0)return 0;
+    int ans = arr[0] + arraysum(arr+1,size-1);
+    return ans;
+}
+//linear search
+int linearsearch(int arr[],int size,int key){
+    if(size==0)return false;
+    if(arr[0]==key)return true;
+    else{
+        return linearsearch(arr+1,size-1,key);
+    }
+}
+//binary search
+bool binarysearch(int arr[],int s,int e,int key){
+    if(s>e)return false;
+    int mid = s + (e-s)/2;
+    if(arr[mid]==key)return true;
+    if(arr[mid]<key)return binarysearch(arr,mid+1,e,key);
+    else return binarysearch(arr,s,mid-1,key);
+}
 int main(){
     cout<<"Recursion"<<endl;
     // int ans = factorial(5);
     // int ans = powerOfTwo(5);
     // countPrinting(5);
-    int n;
-    cin>>n;
+    // int n;
+    // cin>>n;
     // int ans = fib(n);
-    int ans = climbStairs(n);
+    // int ans = climbStairs(n);
+    // cout<< ans << endl;
+
+
+    int arr[5] = {1,2,6,4,5};
+    int size = 5;
+    // bool ans = isSorted(arr,size);
+    // cout<< ans << endl;
+    // int ans = arraysum(arr,size);
+    // cout<< ans << endl;
+    int key = 6;
+    // bool ans = linearsearch(arr,size,key);
+    bool ans = binarysearch(arr,0,size-1,key);
     cout<< ans << endl;
+
     return 0;
 }
